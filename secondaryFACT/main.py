a3f99fd87c81169ecf14d0c777588d14baeed551
Gautham <appskans2017@gmail.com>
2022-01-10 21:28:27 +0530

    secondaryFact

---
+++ secondaryFACT/main.py
@@ -0,0 +1,23 @@
+n = int(input())
+prod = 1
+count = 0
+for i in range(1, n+1):
+    prod *= i
+    if prod == n:
+        k = i
+        if k % 2 == 0:
+            prod1=1
+            for j in range(1, k + 1):
+                if j % 2 == 0:
+                    prod1 *= j
+            print(prod1)
+        elif k % 2 != 0:
+            prod1=1
+            for p in range(1, k + 1):
+                if p % 2 != 0:
+                    prod1 *= p
+            print(prod1)
+    else:
+        count+=1
+        if count==n:
+            print(-1)
